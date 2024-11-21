6ea3533d43d7d7011c6da17d78addb69ccdb3e91
AnhNguyen <nd.anh@hutech.edu.vn>
2024-11-21 10:24:29 +0700

    thêm chức năng abc

---
+++ GradeBookApp/GradeBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBookApp
+{
+    internal class GradeBook
+    {
+        private List<Student> students = new List<Student>();
+
+        public GradeBook() { }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+        public void RemoveStudent(Student student)
+        {
+            students.Remove(student);
+        }
+        // Tìm sinh viên có điểm cao nhất trong danh sách sinh viên
+        public float FindHighestScore()
+        {
+            // sử dụng truy vấn LINQ 
+            return students.Max(s => s.Score);
+        }
+        public float AverageScore()
+        {
+            // sử dụng truy vấn LINQ 
+            return students.Average(s => s.Score);
+        }
+    }
+}
---
+++ GradeBookApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBookApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            GradeBook gradeBook = new GradeBook();
+
+            // Thêm sinh viên vào danh sách
+            while (true)
+            {
+                Console.WriteLine("Nhập điểm sinh viên: ");
+                float score = float.Parse(Console.ReadLine());               
+
+                Student student = new Student
+                {
+                    Score = score
+                };
+
+                gradeBook.AddStudent(student);
+
+                Console.WriteLine("Nhấn phím bất kỳ để tiếp tục, nhấn 'q' để thoát");
+                if (Console.ReadLine().ToLower() == "q")
+                {
+                    break;
+                }
+            }
+
+            // Hiển thị điểm cao nhất và điểm trung bình
+            Console.WriteLine("Điểm cao nhất: " + gradeBook.FindHighestScore());
+            Console.WriteLine("Điểm trung bình: " + gradeBook.AverageScore());
+
+            Console.ReadLine();
+        }
+    }
+}
